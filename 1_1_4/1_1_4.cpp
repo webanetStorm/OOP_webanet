bd3ae4e46e358b7316a57008ee4a2d0d3d02bc1e
webanet <my2712@mail.ru>
2024-02-18 12:22:05 +0300

    Перемещение 1_1_4 в папку OOP_webanet

---
+++ 1_1_4/1_1_4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+
+int main()
+{
+    char value;
+    cin >> value;
+
+
+    if ( value >= 'A' && value <= 'Z' ) 
+        value += 'a' - 'A';
+
+    if ( value >= 'a' && value <= 'z' )
+    {
+        if ( value == 'a' || value == 'e' || value == 'i' || value == 'o' || value == 'u'  || value == 'y' )
+            cout << "vowel";
+        else
+            cout << "consonant";
+    }
+    else
+        cout << "Not letter";
+
+
+    return 0;
+}
