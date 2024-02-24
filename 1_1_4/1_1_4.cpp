a135f7be038d60fe3c8a5af6fcbb2e1dd3ee66fe
webanet <my2712@mail.ru>
2024-02-24 18:12:08 +0300

    1_1_4

--- 1_1_4/1_1_4.cpp
+++ 1_1_4/1_1_4.cpp
@@ -7,23 +7,23 @@ using namespace std;
 
 int main()
 {
-    char value;
-    cin >> value;
+	char value;
+	cin >> value;
+	
+
+	if ( value >= 'A' && value <= 'Z' )
+		value += 'a' - 'A';
+	
+	if ( value >= 'a' && value <= 'z' )
+	{
+		if ( value == 'a' || value == 'e' || value == 'i' || value == 'o' || value == 'u' || value == 'y' )
+			cout << "vowel";
+		else
+			cout << "consonant";
+	}
+	else
+		cout << "Not letter";
 
 
-    if ( value >= 'A' && value <= 'Z' ) 
-        value += 'a' - 'A';
-
-    if ( value >= 'a' && value <= 'z' )
-    {
-        if ( value == 'a' || value == 'e' || value == 'i' || value == 'o' || value == 'u'  || value == 'y' )
-            cout << "vowel";
-        else
-            cout << "consonant";
-    }
-    else
-        cout << "Not letter";
-
-
-    return 0;
+	return 0;
 }
