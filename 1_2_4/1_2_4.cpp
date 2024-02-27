459e5ff46fd58b1fb376346ec54ef144f202530d
webanet <my2712@mail.ru>
2024-02-27 21:02:32 +0300

    1_2_4

---
+++ 1_2_4/1_2_4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include "Calc.h"
+
+using namespace std;
+
+
+int main()
+{
+    Calc calc;
+
+    int a, b;
+    char operation;
+
+
+    cin >> a >> operation >> b;
+    calc.FirstOperation( operation, a, b );
+
+    cin >> operation >> b;
+    calc.NextOperation( operation, b );
+
+    cin >> operation >> b;
+    calc.NextOperation( operation, b );
+
+    cout << calc.GetResult();
+
+
+    return 0;
+}
