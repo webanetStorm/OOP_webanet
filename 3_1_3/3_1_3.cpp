e7f1b5985c341b1ca24daea5bf6ac8d3b81fcdf4
webanet <my2712@mail.ru>
2024-03-11 21:13:47 +0300

    3_1_3 обоссанный

--- 3_1_3/3_1_3.cpp
+++ 3_1_3/3_1_3.cpp
@@ -1,44 +1,42 @@
 ﻿#include <iostream>
 #include <iomanip>
+#include <stdlib.h>
+#include <stdio.h>
 #include "Stack.h"
 
 
 int main()
 {
-    string name;
-    int size1, size2, value;
+	string name1, name2;
+	int size1, size2, value;
 
-    cin >> name >> size1;
-    Stack stack1( name, size1 );
+	cin >> name1 >> size1 >> name2 >> size2;
 
-    cin >> name >> size2;
-    Stack stack2( name, size2 );
+	Stack stack1( name1, size1 ), stack2( name2, size2 );
 
 
-    for ( int i = 0; i < max( size1, size2 ); i++ )
-    {
-        cin >> value;
+	for ( int i = 0; i < max( size1, size2 ); i++ )
+	{
+		cin >> value;
 
-        if ( !stack1.Push( value ) )
-            break;
-        if ( !stack2.Push( value ) )
-            break;
-    }
+		if ( !stack1.Push( value ) )
+			break;
+		if ( !stack2.Push( value ) )
+			break;
+	}
 
 
-    cout << stack1.GetName() << ' ' << stack1.GetSize() << endl;
-    cout << stack2.GetName() << ' ' << stack2.GetSize() << endl;
-    cout << left << setw( 15 ) << stack1.GetName() << stack2.GetName() << endl;
+	cout << stack1.GetName() << ' ' << stack1.GetSize() << endl;
+	cout << stack2.GetName() << ' ' << stack2.GetSize() << endl;
+	cout << left << setw( 15 ) << stack1.GetName() << setw( 15 ) << stack2.GetName() << endl;
 
-    while ( stack1.GetCurrentSize() > 0 || stack2.GetCurrentSize() > 0 )
-    {
-        stack1.Pop( value ) ? cout << right << setw( 15 ) << value : cout << setw( 15 ) << "";
-
-        if ( stack2.Pop( value ) )
-            cout << right << setw( 15 ) << value;
-
-        cout << endl;
-    }
+	while ( stack1.GetCurrentSize() > 0 )
+	{
+		if ( stack1.Pop( value ) )
+			cout << right << setw( 15 ) << value;
+		if ( stack2.Pop( value ) )
+			cout << right << setw( 15 ) << value << endl;
+	}
 
 
 	return 0;
